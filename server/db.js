a10871fe32a8e7afb3f0215cecde68845a4d61c0
bhanu <bhanusree4488@gmail.com>
2026-06-22 11:12:14 +0530

    configure railway databse

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Bhanusree123!@",
-    database: "lms_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
 
