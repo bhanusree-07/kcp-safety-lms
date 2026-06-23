af9037381774c472f3a358582d9cbe841ace7894
bhanu <bhanusree4488@gmail.com>
2026-06-23 12:00:00 +0530

    images

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "localhost",
+    user: "root",
+    password: "Bhanusree123!@",
+    database: "lms_db",
+    port: 3306
 });
 
 db.connect((err) => {
